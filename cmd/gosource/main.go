c0eeac28439ad53d4e47b552a369cfc4949297bf
Joao Regis <joao.reegiis@gmail.com>
2022-03-30 16:38:42 -0300

    added graceful exiting for client execution ...

--- cmd/gosource/main.go
+++ cmd/gosource/main.go
@@ -8,6 +8,8 @@ import (
 	"gosource/internal/hackFunctions/keyboard"
 	"gosource/internal/memory"
 	"gosource/internal/offsets"
+	"os"
+	"os/signal"
 	"time"
 )
 
@@ -15,13 +17,21 @@ var ShouldContinue = true
 
 func main() {
 
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		select {
+		case sig := <-c:
+			fmt.Println("operation aborted by user. closing client.", sig)
+			endCheat()
+			ShouldContinue = false
+			os.Exit(1)
+		}
+	}()
+
 	fmt.Println("getting latest offsets for current game version")
 	offsets.InitOffsets(&configs.Offsets)
-
-	fmt.Println("checking client state: ", configs.Offsets.Signatures.DwClientState)
-	if configs.Offsets.Signatures.DwClientState > 0 {
-		fmt.Println("client state is ok. proceeding.")
-	}
 
 	fmt.Println("initializing key functions")
 	keyboard.InitKeys()
@@ -37,6 +47,9 @@ func main() {
 	// Mainloop
 	fmt.Println("everything is fine. good hacking.")
 	for ShouldContinue {
+
+		go checkCsgoProcess(&ShouldContinue)
+
 		if csgo.UpdatePlayerVars() != nil {
 			continue
 		}
@@ -63,4 +76,17 @@ func main() {
 	}
 
 	fmt.Println("good bye. cya!")
+}
+
+func endCheat() {
+	fmt.Println("clearing client residues ...")
+	features.ClearEngineChams()
+}
+
+func checkCsgoProcess(sc *bool) {
+	if !memory.Init() {
+		fmt.Println("game has been closed.")
+		endCheat()
+		*sc = false
+	}
 }
