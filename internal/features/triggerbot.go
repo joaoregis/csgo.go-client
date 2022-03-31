aef189b40ecf6f2879b2b3b918a4a9d835a43498
Joao Regis <joao.reegiis@gmail.com>
2022-03-31 20:48:17 -0300

    added triggerbot fire delay

--- internal/features/triggerbot.go
+++ internal/features/triggerbot.go
@@ -5,7 +5,11 @@ import (
 	"gosource/internal/csgo"
 	kb "gosource/internal/hackFunctions/keyboard"
 	"gosource/internal/memory"
+	"time"
 )
+
+var triggerbotDelayCounter time.Time
+var triggerbotDelayCounter_Registered bool = false
 
 func Triggerbot() {
 
@@ -25,10 +29,16 @@ func Triggerbot() {
 		return
 	}
 
+	isHotkeyPressed := kb.GetAsyncKeyState(kb.GetKey(c.G.Triggerbot.Key))
+
+	if !isHotkeyPressed {
+		triggerbotDelayCounter_Registered = false
+		return
+	}
+
 	isEnemyTarget, _ := csgo.PlayerIsEnemy(targetEntityAtCrosshair)
 	isCrosshairIdGreaterThanZero := crosshairTargetId > 0
 	isCrosshairIdLowerThanMaxPlayers := int64(crosshairTargetId) <= int64(csgo.MaxPlayers)
-	isHotkeyPressed := kb.GetAsyncKeyState(kb.GetKey(c.G.Triggerbot.Key))
 	isValidPlayer, _ := csgo.PlayerIsValid(targetEntityAtCrosshair)
 	isImmune, _ := csgo.IsEntityImmune(targetEntityAtCrosshair)
 
@@ -42,7 +52,33 @@ func Triggerbot() {
 
 	if shouldShot {
 
+		if c.G.Triggerbot.Delay > 0 {
+			if !triggerbotDelayCounter_Registered {
+
+				triggerbotDelayCounter_Registered = true
+				triggerbotDelayCounter = time.Now()
+
+				// return to check in the next frame if time elapsed had passed
+				return
+
+			} else {
+				elapsed := time.Since(triggerbotDelayCounter).Milliseconds()
+				if elapsed <= int64(c.G.Triggerbot.Delay) {
+					// return to prevent trigger from firing before estabilished delay
+					return
+				} else {
+					triggerbotDelayCounter_Registered = false
+				}
+			}
+		}
+
+		memory.GameProcess.Write(memory.GameClient+c.Offsets.Signatures.DwForceAttack, "int", 4)
 		memory.GameProcess.Write(memory.GameClient+c.Offsets.Signatures.DwForceAttack, "int", 6)
+
+	} else {
+
+		triggerbotDelayCounter_Registered = false
+
 	}
 
 }
