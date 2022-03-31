4f6f3f7e17d1c120c803e142b2d774da51c8fdaa
Joao Regis <joao.reegiis@gmail.com>
2022-03-31 20:48:42 -0300

    added WIP autoWeapon system

--- cmd/gosource/main.go
+++ cmd/gosource/main.go
@@ -16,6 +16,8 @@ import (
 var ShouldContinue = true
 var found = false
 
+// var t time.Time
+
 func main() {
 
 	c := make(chan os.Signal)
@@ -59,7 +61,6 @@ func main() {
 
 				if tries > 10 {
 					panic("could not find updated offsets ...")
-					return
 				}
 
 				fmt.Println("trying to recover updated offsets ...")
@@ -88,7 +89,26 @@ func main() {
 		features.BunnyHop()
 		features.Visuals()
 
-		go features.Aimbot()
+		features.Aimbot()
+
+		// // TODO:
+		/*
+			> check equipped weapon to make sure to allow this only when weapon is not automatic
+			> check when revolver is equipped to prevent from "stuck" revolver firing
+			> check if game is foreground to prevent from fire when game is minimized
+
+		*/
+		// if configs.G.AutoWeapons.Enabled {
+		// 	elapsed := time.Since(t).Milliseconds()
+		// 	// 15ms is the minimum value and 25ms is the "maximum" minimum value to make it random
+		// 	if elapsed > int64(rand.Intn(25-15)+15)+int64(configs.G.AutoWeapons.Delay) {
+		// 		if keyboard.GetAsyncKeyState(keyboard.GetKey("Mouse 1")) {
+		// 			t = time.Now()
+		// 			memory.GameProcess.Write(memory.GameClient+configs.Offsets.Signatures.DwForceAttack, "int", 4)
+		// 			memory.GameProcess.Write(memory.GameClient+configs.Offsets.Signatures.DwForceAttack, "int", 6)
+		// 		}
+		// 	}
+		// }
 
 		if !ShouldContinue {
 			break
