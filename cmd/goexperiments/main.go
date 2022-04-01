1df8aee662c5ac3a43fa8da21d685cf9d154a31b
Joao Regis <joao.reegiis@gmail.com>
2022-04-01 16:28:12 -0300

    channel experiments

--- cmd/goexperiments/main.go
+++ cmd/goexperiments/main.go
@@ -1,56 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-	"syscall"
-	"unsafe"
-
-	"golang.org/x/sys/windows"
-)
-
-var (
-	mod                     = windows.NewLazyDLL("user32.dll")
-	procGetWindowText       = mod.NewProc("GetWindowTextW")
-	procGetWindowTextLength = mod.NewProc("GetWindowTextLengthW")
-)
-
-type (
-	HANDLE uintptr
-	HWND   HANDLE
-)
-
-func GetWindowTextLength(hwnd HWND) int {
-	ret, _, _ := procGetWindowTextLength.Call(
-		uintptr(hwnd))
-
-	return int(ret)
-}
-
-func GetWindowText(hwnd HWND) string {
-	textLen := GetWindowTextLength(hwnd) + 1
-
-	buf := make([]uint16, textLen)
-	procGetWindowText.Call(
-		uintptr(hwnd),
-		uintptr(unsafe.Pointer(&buf[0])),
-		uintptr(textLen))
-
-	return syscall.UTF16ToString(buf)
-}
-
-func GetWindow(funcName string) uintptr {
-	proc := mod.NewProc(funcName)
-	hwnd, _, _ := proc.Call()
-	return hwnd
-}
+import "os"
 
 func main() {
-	for {
-		if hwnd := GetWindow("GetForegroundWindow"); hwnd != 0 {
-			text := GetWindowText(HWND(hwnd))
 
-			fmt.Println(strings.Contains(text, "Counter-Strike"))
-		}
-	}
+	os.Exit(1)
 }
