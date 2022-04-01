da979acc7f75d2746b8676201eeea925944a77ea
Joao Regis <joao.reegiis@gmail.com>
2022-04-01 18:21:43 -0300

    added weapon handler new modules

--- internal/csgo/entity.go
+++ internal/csgo/entity.go
@@ -2,6 +2,7 @@ package csgo
 
 import (
 	"gosource/internal/configs"
+	"gosource/internal/csgo/weapons"
 	"gosource/internal/hackFunctions/vector"
 	"gosource/internal/memory"
 	"math"
@@ -52,6 +53,204 @@ func GetLocalPlayer() (uintptr, error) {
 	}
 
 	return player, nil
+}
+
+func GetActiveWeapon() (int, error) {
+
+	dwLocalPlayer, _ := GetLocalPlayer()
+	hActiveWeapon, _ := memory.GameProcess.ReadIntPtr(dwLocalPlayer + configs.Offsets.Netvars.MHActiveWeapon)
+	dwWeaponEntity, _ := memory.GameProcess.ReadIntPtr(memory.GameClient + configs.Offsets.Signatures.DwEntityList + ((hActiveWeapon&0xFFF)-1)*0x10)
+	nWeaponIndex, _ := memory.GameProcess.ReadInt(dwWeaponEntity + configs.Offsets.Netvars.MIItemDefinitionIndex)
+	return nWeaponIndex & 0xFFF, nil
+
+}
+
+func IsC4Item(wIdx int) bool {
+
+	return wIdx == weapons.C4
+
+}
+
+func IsGranade(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Flashbang,
+		weapons.HeGrenade,
+		weapons.SmokeGrenade,
+		weapons.Molotov,
+		weapons.Decoy,
+		weapons.IncGrenade,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsAimbotable(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Usp_s,
+		weapons.Glock,
+		weapons.Fiveseven,
+		weapons.Cz75a,
+		weapons.P250,
+		weapons.Hkp2000,
+		weapons.Tec9,
+		weapons.Deagle,
+		weapons.Revolver,
+		weapons.Awp,
+		weapons.Ssg08,
+		weapons.Mac10,
+		weapons.P90,
+		weapons.Mp5sd,
+		weapons.Ump45,
+		weapons.Bizon,
+		weapons.Mp7,
+		weapons.Mp9,
+		weapons.Ak47,
+		weapons.M4a1_s,
+		weapons.M4A4,
+		weapons.Famas,
+		weapons.Aug,
+		weapons.GalilAr,
+		weapons.Sg553,
+		weapons.M249,
+		weapons.Xm1014,
+		weapons.Mag7,
+		weapons.Negev,
+		weapons.Sawedoff,
+		weapons.Nova,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsPistol(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Usp_s,
+		weapons.Glock,
+		weapons.Fiveseven,
+		weapons.Cz75a,
+		weapons.P250,
+		weapons.Hkp2000,
+		weapons.Tec9,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsBoltSniper(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Awp,
+		weapons.Ssg08,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsSubmachineGun(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Mac10,
+		weapons.P90,
+		weapons.Mp5sd,
+		weapons.Ump45,
+		weapons.Bizon,
+		weapons.Mp7,
+		weapons.Mp9,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsAutoSniper(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Scar20,
+		weapons.G3SG1,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsSniper(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Awp,
+		weapons.Ssg08,
+		weapons.Scar20,
+		weapons.G3SG1,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsHeavyPistol(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Deagle,
+		weapons.Revolver,
+	}
+
+	return contains(items, wIdx)
+
+}
+
+func IsDeagle(wIdx int) bool {
+
+	return wIdx == weapons.Deagle
+
+}
+
+func IsRevolver(wIdx int) bool {
+
+	return wIdx == weapons.Revolver
+
+}
+
+func IsTaser(wIdx int) bool {
+
+	return wIdx == weapons.Taser
+
+}
+
+func IsKnife(wIdx int) bool {
+
+	isInKnifeModelRange := wIdx >= weapons.KnifeBayonet && wIdx <= weapons.KnifeSkeleton
+
+	var items []int = []int{
+		weapons.KnifeCT,
+		weapons.KnifeT,
+		weapons.GhostKnife,
+		weapons.GoldenKnife,
+	}
+
+	return contains(items, wIdx) || isInKnifeModelRange
+
+}
+
+func IsRifle(wIdx int) bool {
+
+	var items []int = []int{
+		weapons.Ak47,
+		weapons.M4a1_s,
+		weapons.M4A4,
+		weapons.Famas,
+		weapons.Aug,
+		weapons.GalilAr,
+		weapons.Sg553,
+	}
+
+	return contains(items, wIdx)
+
 }
 
 func GetMaxPlayers() (int, error) {
@@ -179,4 +378,13 @@ func GetBonePos(entity uintptr, bone int) (vector.Vector3, error) {
 	}
 
 	return vector.Vector3{X: x, Y: y, Z: z}, nil
+}
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
