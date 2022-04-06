784352dd99b307908e8b8da0ed830859cb37d8ed
Joao Regis <joao.reegiis@gmail.com>
2022-04-06 15:58:05 -0300

    fix build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $rndName = -join ((65..90) + (97..122) | Get-Random -Count 16 | ForEach-Object {[char]$_})
 
-go build -o bin/$rndName.exe -ldflags='-X main.BUILD_TIMESTAMP="'$(Get-Date -f yyyyMMddHHmmss)'"' ./cmd/gosource/main.go 
+go build -o bin/$rndName.exe -ldflags='-X main.BUILD_TIMESTAMP="'$(Get-Date -f yyyyMMddHHmmss)'"' ./cmd/gosource 
 
 Write-Host "Output: bin/$($rndName).exe"
