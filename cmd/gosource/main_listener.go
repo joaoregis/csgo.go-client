cb3cc576680696ae438d4ef96d88148fe8a92c00
Joao Regis <joao.reegiis@gmail.com>
2022-04-09 02:20:26 -0300

    add update new action to put values in memory config

--- cmd/gosource/main_listener.go
+++ cmd/gosource/main_listener.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"gosource/internal/global/configs"
 	"gosource/internal/global/logs"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,18 +14,34 @@ func initListener() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/orchestrator/request-action/{action}", requestRefreshConfig).Methods("GET")
+	router.HandleFunc("/orchestrator/request-action/{action}", requestRefreshConfig_GET).Methods("GET")
+	router.HandleFunc("/orchestrator/request-action/{action}", requestRefreshConfig_PUT).Methods("PUT")
 
 	logs.Error(http.ListenAndServe(":61975", router))
 
 }
 
-func requestRefreshConfig(w http.ResponseWriter, r *http.Request) {
+func requestRefreshConfig_GET(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	action := params["action"]
 	switch action {
 	case "reload-cfg":
 		configs.Reload()
+	default:
+		w.Write([]byte("[\"invalid action\"]"))
+	}
+}
+
+func requestRefreshConfig_PUT(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	action := params["action"]
+	switch action {
+	case "update-option":
+
+		bs, _ := ioutil.ReadAll(r.Body)
+		json.Unmarshal(bs, &configs.G)
+		logs.Info("config option updated")
+
 	default:
 		w.Write([]byte("[\"invalid action\"]"))
 	}
